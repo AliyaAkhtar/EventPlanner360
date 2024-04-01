2db624e8646463d2b5f33c51b499898866591bcd
AliyaAkhtar <aliya10akhtar3a@gmail.com>
2024-04-01 15:09:26 +0500

    first commit

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Hero = () => {
+    return (
+        <section className="hero">
+            {/* Your hero section content goes here */}
+            <div className="hero-content">
+                <h1>Welcome to EventPlanner360</h1>
+                <p>Your ultimate solution for seamless event planning</p>
+                <button>Get Started</button>
+            </div>
+        </section>
+    );
+};
+
+export default Hero
